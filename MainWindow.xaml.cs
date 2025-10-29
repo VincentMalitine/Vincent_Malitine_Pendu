1956069b82d977ff96430613aef628db2b683fd9
vincent malitine <vincent.malitine@saintmichelannecy.fr>
2025-10-30 00:02:43 +0100

    Ajoutez des fichiers projet.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Pendu_Vincent_Malitine
+{
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+    {
+        string mot = " "; // Le mot à deviner
+        string[] listmots = { "tuile", "bananaleclerc", "prototype", "nurburgring", "nordschleiffe" }; // Liste de mots possibles
+        int vie = 6; // Nombre d'essais restants
+        string lettresDevinees = ""; // Lettres déjà devinées
+        bool jeuTermine = false; // Indique si le jeu est terminé
+        bool IsWordGuessed = false;
+        char TextBox_Result = ' ';
+        char tentative = ' ';
+
+        
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            while (jeuTermine == false)
+            {
+                Random rand = new Random();
+                int index = rand.Next(listmots.Length);
+                mot = listmots[index];
+            }
+        }
+
+        private void Button_Letter_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button btn && btn.Content is string content && content.Length == 1)
+            {
+                TextBox_Result = content[0];
+                ResultTextBox.Text = TextBox_Result.ToString();
+            }
+        }
+
+        private void Button_Done_Click(object sender, RoutedEventArgs e)
+        {
+            tentative = TextBox_Result;
+            TextBox_Result = ' ';
+            if (mot.Contains(tentative))
+            {
+                lettresDevinees += tentative;
+                if (IsWordGuessed = true)
+                {
+                    MessageBox.Show("Félicitations ! Vous avez deviné le mot : " + mot);
+                    jeuTermine = true;
+                }
+            }
+            else
+            {
+                vie--;
+                LifeTextBox.Text = "Vies restantes : " + vie;
+                if (vie <= 0)
+                {
+                    MessageBox.Show("Game Over ! Le mot était : " + mot);
+                    jeuTermine = true;
+                }
+            }
+        }
+    }
+}
